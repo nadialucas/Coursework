f8cb0ee6f01eab2660af5e3b5a89d15b466a9c96
Nadia Lucas <nlucas040@gmail.com>
2020-11-27 15:21:31 -0600

    betas and gammas done

--- Metrics1/Lucas_pset3/pset2_problem6.py
+++ Metrics1/Lucas_pset3/pset2_problem6.py
@@ -7,7 +7,8 @@ import numpy as np
 import pandas as pd 
 import scipy.integrate as integrate
 import math
-
+import gurobipy as gp
+from gurobipy import GRB
 
 D = np.array([.12, .29, .48, .78])
 D = D.reshape((len(D), 1))
@@ -16,10 +17,18 @@ Z = Z.reshape((len(Z), 1))
 
 
 
-class IV_estimand:
+class estimand:
 	def __init__(self, D, Z):
 		self.D = D
 		self.Z = Z
+		# sanity check that these match up with figure 4
+		self.w_iv0, self.w_iv1 = self.w_iv()
+		self.w_tsls0, self.w_tsls1 = self.w_tsls()
+		self.w_att0, self.w_att1 = self.w_att()
+		# uses all the helper function to return the estimands
+		self.beta_iv = self.beta_s(self.w_iv0, self.w_iv1)
+		self.beta_tsls = self.beta_s(self.w_tsls0, self.w_tsls1)
+		self.beta_att = self.beta_s(self.w_att0, self.w_att1, True)
 
 	def m0(self, u):
 		return 0.9 - 1.1*u + 0.3*u**2
@@ -27,17 +36,26 @@ class IV_estimand:
 	def m1(self, u):
 		return 0.35 - 0.3*u - 0.05*u**2
 
-	def s_tsls(self):
+	def w_tsls(self):
+		weights0 = []
+		weights1 = []
 		Ztilde = np.diag(np.ones(len(self.Z)))
 		Xtilde = np.hstack((np.ones((len(self.D), 1)),self.D))
 
 		EZX = np.dot(Ztilde.T, Xtilde)/len(self.Z)
 		EZZ = np.dot(Ztilde.T, Ztilde)/len(self.Z)
 		Pi = np.dot(EZX.T, np.linalg.inv(EZZ))
-		return(np.dot(np.dot(np.linalg.inv(np.dot(Pi, EZX)), Pi), Ztilde)[1])
+		s = np.dot(np.dot(np.linalg.inv(np.dot(Pi, EZX)), Pi), Ztilde)[1]
+		for j in range(len(self.D)):
+			weights0.append(sum(s[:j+1])/len(self.D))
+			weights1.append(sum(s[j:])/len(self.D))
+		return weights0, weights1
 
-	def s_iv(self):
+
+	def w_iv(self):
 		s_list = []
+		weights0 = []
+		weights1 = []
 		EDZ = np.dot(self.D.T, self.Z)[0]/len(self.Z)
 		EZ = np.sum(self.Z)/len(self.Z)
 		ED = np.sum(self.D)/len(self.D)
@@ -45,76 +63,38 @@ class IV_estimand:
 		for z in self.Z:
 			s=(z[0]-EZ)/CovDZ
 			s_list.append(s)
-		return s_list
-
-	def weights(self):
-		s0 = self.s_tsls()[0]
-		s1 = self.s_tsls()[1]
-		w0 = [sum(s0)/4.0, sum(s0[1:])/3.0, sum(s0[2:])/2.0]
+		for j in range(len(self.D)):
+			weights0.append(sum(s_list[:j+1])/len(self.D))
+			weights1.append(sum(s_list[j:])/len(self.D))
+		return weights0, weights1
 
 	def w_att(self):
-		w_list = []
+		ED = np.sum(self.D)/len(self.D)
+		weights0 = np.multiply((-1.0/ED), np.array([1,.75, .5, .25]))
+		weights1 = np.multiply((1.0/ED), np.array([1,.75, .5, .25]))
+		return weights0, weights1
+
+	def beta_s(self, weights0, weights1, att = False):
+		beta_s = 0
 		Dflat = self.D.flatten()
-		for i in range(len(self.D)):
-			w1 = sum(Dflat[i:])/len(self.D)
-			w_list.append(1/Dflat[i])
-		return w_list
-
-	def beta_s0(self, s):
-		beta_s = 0
-		weights0 = []
-		for i in range(len(self.D)):
-			w0 = sum(s[:i+1])/len(self.D)
-			integral = integrate.quad(self.m0, D[i], 1)
-			beta_s += w0*integral[0]
-			weights0.append(w0)
-		# sanity check that these weights line up with figure 4
-		print(weights0)
+		Dflat = np.insert(Dflat, 0, 0)
+		Dflat = np.insert(Dflat, len(self.D)+1, 1)
+		for i in list(range(1, len(self.D)+1)):
+			w0 = weights0[i-1]
+			w1 = weights1[i-1]
+			if att:
+				integral0 = integrate.quad(self.m0, Dflat[i-1], Dflat[i])[0]
+			else:
+				integral0 = integrate.quad(self.m0, Dflat[i], Dflat[i+1])[0]
+			integral1 = integrate.quad(self.m1, Dflat[i-1], Dflat[i])[0]
+			beta_s += (w0*integral0 + w1*integral1)
 		return beta_s
 
-	def beta_s1(self, s):
-		beta_s = 0
-		weights1 = []
-		for i in range(len(self.D)):
-			w1 = sum(s[i:])/len(self.D)
-			weights1.append(w1)
-			integral = integrate.quad(self.m1, 0, D[i])
-			beta_s += w1*integral[0]
-		# sanity check that these weights lineup with figure 4
-		print(weights1)
-		return beta_s
 
-	def beta_tsls(self):
-		s = self.s_tsls()
-		beta_s0 = self.beta_s0(s)
-		beta_s1 = self.beta_s1(s)		
-		return beta_s0 + beta_s1
-
-	def beta_iv(self):
-		s = self.s_iv()
-		print(s)
-		print("hi")
-		beta_s0 = self.beta_s0(s)
-		beta_s1 = self.beta_s1(s)		
-		return beta_s0 + beta_s1
-
-	def beta_att(self):
-		weights_att = self.w_att()
-		yo = self.beta_s1(weights_att)
-		print("yoo")
-		print(yo)
-		beta_s = 0
-		for i in range(len(self.D)):
-			w1 = weights_att[i]
-			integral = integrate.quad(self.m1, 0, D[i])
-			beta_s += w1*integral[0]
-		print(weights_att)
-		return beta_s
-
-yo = IV_estimand(D,Z)
-# print(yo.beta_iv())
-# print(yo.beta_tsls())
-# print(yo.beta_att())
+yo = estimand(D,Z)
+print(yo.beta_iv)
+print(yo.beta_tsls)
+print(yo.beta_att)
 
 # construct the gammas
 class gamma:
@@ -122,15 +102,68 @@ class gamma:
 		self.D = D
 		self.Z = Z
 		self.K = K
-
-	# returns list of coefficients
-	def bernstein(self):
-		yo = []
+		Dflat = self.D.flatten()
+		Dflat = np.insert(Dflat, 0, 0)
+		self.Dflat = np.insert(Dflat, len(self.D)+1, 1)
+		coeflist = []
+		powerlist = []
 		for k in range(self.K):
 			for i in list(range(k, self.K+1)):
-				yo.append((-1)**(i-k) * math.comb(self.K, i) * math.comb(i, k))
-		print(yo)
+				coeflist.append((-1)**(i-k) * math.comb(self.K, i) * math.comb(i, k))
+				powerlist.append(i)
+		self.coeflist = np.array(coeflist)
+		self.powerlist = np.array(powerlist)
+		self.estimate = estimand(D, Z)
+
+	# uses list of coefficients and powers to construct a function
+	def bernstein(self, u, k):
+		#return np.sum(np.multiply(self.coeflist, np.power(u, self.powerlist)))
+		return math.comb(self.K, k)*(u**k)*((1-u)**(self.K-k))
 
 
-hi = gamma(D, Z, 3)
-hi.bernstein()
+	def gamma_star(self, k):
+		gamma_s0 = 0
+		gamma_s1 = 0
+		for i in list(range(1, len(self.D)+1)):
+			w0 = self.estimate.w_att0[i-1]
+			w1 = self.estimate.w_att1[i-1]
+			integral = integrate.quad(self.bernstein, self.Dflat[i-1], self.Dflat[i], args = k)[0]
+			gamma_s0 += w0*integral
+			gamma_s1 += w1*integral
+		return(gamma_s0, gamma_s1)
+
+	def gamma_iv(self, k):
+		gamma_s0 = 0
+		gamma_s1 = 0
+		for i in list(range(1, len(self.D)+1)):
+			w0 = self.estimate.w_iv0[i-1]
+			w1 = self.estimate.w_iv1[i-1]
+			integral0 = integrate.quad(self.bernstein, self.Dflat[i], self.Dflat[i+1], args=k)[0]
+			integral1 = integrate.quad(self.bernstein, self.Dflat[i-1], self.Dflat[i], args = k)[0]
+			gamma_s0 += w0*integral0
+			gamma_s1 += w1*integral1
+		return(gamma_s0, gamma_s1)
+
+	def gamma_tsls(self, k):
+		gamma_s0 = 0
+		gamma_s1 = 0
+		for i in list(range(1, len(self.D)+1)):
+			w0 = self.estimate.w_tsls0[i-1]
+			w1 = self.estimate.w_tsls1[i-1]
+			integral0 = integrate.quad(self.bernstein, self.Dflat[i], self.Dflat[i+1], args=k)[0]
+			integral1 = integrate.quad(self.bernstein, self.Dflat[i-1], self.Dflat[i], args = k)[0]
+			gamma_s0 += w0*integral0
+			gamma_s1 += w1*integral1
+		return(gamma_s0, gamma_s1)
+
+
+
+
+hi = gamma(D, Z, 1)
+print(hi.gamma_star(0))
+print(hi.gamma_star(1))
+print(hi.gamma_iv(0))
+print(hi.gamma_iv(1))
+print(hi.gamma_tsls(0))
+print(hi.gamma_tsls(1))
+
