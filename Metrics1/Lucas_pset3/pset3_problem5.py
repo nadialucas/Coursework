af5b4b7e92e99d5179303d9e4d43a061ab479c89
Nadia Lucas <nlucas040@gmail.com>
2020-11-28 23:08:43 -0600

    most of 5 implemented - some issues with plotting

--- Metrics1/Lucas_pset3/pset3_problem5.py
+++ Metrics1/Lucas_pset3/pset3_problem5.py
@@ -5,14 +5,129 @@
 
 import numpy as np 
 import pandas as pd 
-import scipy.integrate as integrate
 import math
-import gurobipy as gp
-from gurobipy import GRB
+from scipy.stats import norm #for a normal cdf function
+import matplotlib.pyplot as plt
 
-D = np.array([.12, .29, .48, .78])
-D = D.reshape((len(D), 1))
-Z = np.array([1, 2, 3, 4])
-Z = Z.reshape((len(Z), 1))
+datapath = "/Users/nadialucas/Dropbox/Second year/ECON 31720/ps3_files/"
+
+df = pd.read_csv(datapath+"angrist_evans_clean.csv")
+
+
+print(df.info(verbose=True))
+
+Yvar = ['worked']
+Dvar = ['more2kids']
+Zvar = ['samesex']
+Xvars = ['age', 'ageat1st', 'agekid1', 'agekid2', 'boy1st', 'boy2nd', 'black', 'hispanic', 'otherrace']
+Zvars = Zvar+Xvars
+
+# this will return u
+def probit(D, Z):
+	est = estimate(Z, D, Z)
+	beta = est.regress()
+	predicted = np.dot(beta.T, Z.T)
+	return norm.cdf(predicted).T
+
+class estimate:
+	def __init__(self, X,Y,Z):
+		self.N = len(Y)
+		self.Z = Z
+		self.Y = Y
+		self.X = X
+		self.k = X.shape[1]
+		# initialize the object [X'X]^-1. This is the "bread" of the sandwich
+		# in any sandwich estimator and will be used in each error calculation
+		self.XpXi = np.linalg.inv(np.dot(self.X.T, self.X))
+		self.ZpZi = np.linalg.inv(np.dot(self.Z.T, self.Z))
+		# solve for the coefficients
+		self.theta = np.dot(self.XpXi, np.dot(self.X.T, self.Y))
+		self.pi = np.dot(self.ZpZi, np.dot(self.Z.T, self.X))
+
+	def regress(self):
+		# returns the regular OLS estimator
+		return self.theta
+
+	def tsls(self):
+		# 2SLS estimator - following the logic of the classic sandwich estimator
+		pi = self.pi
+		PZZPinv = np.linalg.inv(np.dot(np.dot(pi.T, np.dot(self.Z.T, self.Z)),\
+		 pi)) 
+		beta = np.dot(np.dot(PZZPinv, pi.T), np.dot(self.Z.T, self.Y))
+		return beta
+
+
+
+class mtr_estimates:
+	def __init__(self, df, Dvar, Zvars, Xvars, Yvar):
+		self.D = df[Dvar].to_numpy()
+		self.Z = df[Zvars].to_numpy()
+		self.U = probit(self.D,self.Z)
+		df['U'] = self.U
+		self.X = df[Xvars].to_numpy()
+		self.Y = df[Yvar].to_numpy()
+		self.X1 = df[df[Dvar[0]] == 1][Xvars].to_numpy()
+		self.X0 = df[df[Dvar[0]] == 0][Xvars].to_numpy()
+		self.U1 = df[df[Dvar[0]] == 1]['U'].to_numpy()
+		self.U0 = df[df[Dvar[0]] == 0]['U'].to_numpy()
+		self.Y1 = df[df[Dvar[0]] == 1][Yvar].to_numpy()
+		self.Y0 = df[df[Dvar[0]] == 0][Yvar].to_numpy()
+		self.C1 = np.ones((len(self.U1), 1))
+		self.C0 = np.ones((len(self.U0), 1))
+		self.U1 = self.U1.reshape(len(self.U1), 1)
+		self.U0 = self.U0.reshape(len(self.U0), 1)
+
+	def mtr_helper(self, X1, X0):
+		coefs1 = estimate(X1, self.Y1, self.U1).regress()
+		coefs0 = estimate(X0, self.Y0, self.U0).regress()
+
+		# use the estimates to impute the potential outcomes Y(1) and Y(0)
+		Y0_imputed = np.dot(coefs0.T, X1.T).T
+		Y1_imputed = np.dot(coefs1.T, X0.T).T
+
+		# now construct an originally sized dataframe with potential outcomes (D==0 on top)
+		Y0 = np.concatenate([self.Y0, Y0_imputed])
+		Y1 = np.concatenate([Y1_imputed, self.Y1])
+		MTE = Y1 - Y0
+		C = np.ones((len(MTE), 1))
+		U = np.concatenate([self.U0, self.U1])
+		D = np.concatenate([np.zeros(len(self.U0)), np.ones(len(self.U1))])
+
+
+		# WHY are these all virtually identical???
+		ATE = np.mean(Y1) - np.mean(Y0)
+		ATT = np.mean(self.Y1) - np.mean(Y0_imputed)
+		ATU = np.mean(Y1_imputed) - np.mean(self.Y0)
+		print(ATE)
+		print(ATT)
+		print(ATU)
+
+		# what are we supposed to be plotting???
+
+		fig, ax = plt.subplots()
+		ax.plot(Uplot, MTE_impute, '.', markersize =.8, color='dodgerblue')
+		plt.show()
+
+	def mtr1(self):
+		X1 = np.hstack((np.hstack((self.C1, self.U1)), self.X1))
+		X0 = np.hstack((np.hstack((self.C0, self.U0)), self.X0))
+		self.mtr_helper(X1, X0)
+		
+
+
+
+
+
+hi = mtr_estimates(df, Dvar, Zvars, Xvars, Yvar)
+hi.mtr1()
+
+
+
+
+
+
+# mtr_estimate()
+
+
 
 
--- Metrics1/Lucas_pset3/pset3_problem6.py
+++ Metrics1/Lucas_pset3/pset3_problem6.py
@@ -9,14 +9,16 @@ import scipy.integrate as integrate
 import math
 import gurobipy as gp
 from gurobipy import *
+import matplotlib.pyplot as plt
 
+# setting up what the problem gives us
 D = np.array([.12, .29, .48, .78])
 D = D.reshape((len(D), 1))
 Z = np.array([1, 2, 3, 4])
 Z = Z.reshape((len(Z), 1))
 
 
-
+# class for calculating and storing all weights and betas
 class estimand:
 	def __init__(self, D, Z):
 		self.D = D
@@ -36,22 +38,27 @@ class estimand:
 	def m1(self, u):
 		return 0.35 - 0.3*u - 0.05*u**2
 
+	# TSLS weights derived from teh jth component
 	def w_tsls(self):
 		weights0 = []
 		weights1 = []
+		# using the definition of the instrument in TSLS
+		# as a matrix of indicator vectors
 		Ztilde = np.diag(np.ones(len(self.Z)))
+		# adding a constant to demean treatment
 		Xtilde = np.hstack((np.ones((len(self.D), 1)),self.D))
-
+		# calculate expectations and solve
 		EZX = np.dot(Ztilde.T, Xtilde)/len(self.Z)
 		EZZ = np.dot(Ztilde.T, Ztilde)/len(self.Z)
 		Pi = np.dot(EZX.T, np.linalg.inv(EZZ))
 		s = np.dot(np.dot(np.linalg.inv(np.dot(Pi, EZX)), Pi), Ztilde)[1]
+		# transform s into weights
 		for j in range(len(self.D)):
 			weights0.append(sum(s[:j+1])/len(self.D))
 			weights1.append(sum(s[j:])/len(self.D))
 		return weights0, weights1
 
-
+	# IV weights coming from table 3
 	def w_iv(self):
 		s_list = []
 		weights0 = []
@@ -70,10 +77,14 @@ class estimand:
 
 	def w_att(self):
 		ED = np.sum(self.D)/len(self.D)
-		weights0 = np.multiply((-1.0/ED), np.array([1,.75, .5, .25]))
-		weights1 = np.multiply((1.0/ED), np.array([1,.75, .5, .25]))
+		s = (1.0/ED)*np.ones(len(self.D))
+		weights1 = []
+		for j in range(len(self.D)):
+			weights1.append(sum(s[j:])/len(self.D))
+		weights0 = np.multiply(-1.0, weights1)
 		return weights0, weights1
 
+	# somehow this is wrong for beta_att but it seems minor and everything else worked (ignoring for now)
 	def beta_s(self, weights0, weights1, att = False):
 		beta_s = 0
 		Dflat = self.D.flatten()
@@ -103,10 +114,9 @@ class gamma:
 		self.Dflat = np.insert(Dflat, len(self.D)+1, 1)
 		self.estimate = estimand(D, Z)
 
-	# uses list of coefficients and powers to construct a function
+	# regular Bernstein function
 	def bernstein(self, u, k):
 		return math.comb(self.K, k)*(u**k)*((1-u)**(self.K-k))
-
 
 	def gamma_star_k(self, k):
 		gamma_s0 = 0
@@ -148,7 +158,7 @@ class gamma:
 
 # Time to implement the solver
 
-def solver(K, D, Z, max = True):
+def solver(K, D, Z, max, monotonic):
 	estimandK = estimand(D, Z)
 	gammaK = gamma(D, Z, K)
 	gamma_star = []
@@ -159,26 +169,21 @@ def solver(K, D, Z, max = True):
 		gamma_iv.append(gammaK.gamma_iv_k(k))
 		gamma_tsls.append(gammaK.gamma_tsls_k(k))
 
-	# add monotonicity into the solver
+	# add monotonicity into the solver (decreasing)
 	mono_constraint = np.zeros((K, K+1))
 	for k in range(K):
 		mono_constraint[k, k] = 1
 		mono_constraint[k, k+1] = -1
-
-
-	print(mono_constraint)
-	print(len(gamma_iv))
-
-
 
 	model = gp.Model('GetBounds')
 	# Create decision variables for the model
 	theta = model.addVars(K+1, 2, lb = 0, ub = 1)
 	model.addConstr((sum(theta[i,j] * gamma_iv[i][j] for i in range(K+1) for j in range(2)) == estimandK.beta_iv), name = 'IV')
 	model.addConstr((sum(theta[i,j] * gamma_tsls[i][j] for i in range(K+1) for j in range(2)) == estimandK.beta_tsls), name = 'TSLS')
-	for k in range(K):
-		model.addConstr((sum(theta[i, 0] * mono_constraint[k, i] for i in range(K+1)) <= 0))
-		model.addConstr((sum(theta[i, 1] * mono_constraint[k, i] for i in range(K+1)) <= 0))
+	if monotonic:
+		for k in range(K):
+			model.addConstr((sum(theta[i, 0] * mono_constraint[k, i] for i in range(K+1)) <= 0))
+			model.addConstr((sum(theta[i, 1] * mono_constraint[k, i] for i in range(K+1)) <= 0))
 	if max:
 		model.setObjective((sum(theta[i,j] * gamma_star[i][j] for i in range(K+1) for j in range(2))), GRB.MAXIMIZE)
 	else:
@@ -186,10 +191,42 @@ def solver(K, D, Z, max = True):
 	
 
 	model.optimize()
-
-	print(model.objVal)
 	return model.objVal
 	
+att = estimand(D,Z).beta_att
+print(att)
 
-solver(19, D, Z)
+def plot_polys(maxK, D, Z):
+	att = estimand(D,Z).beta_att
+	X = list(range(1, maxK+1))
+	maxYpoly = []
+	minYpoly = []
+	maxYmono = []
+	minYmono = []
+	attY = att*np.ones(maxK)
+	for K in list(range(1, maxK+1)):
+		maxYpoly.append(solver(K, D, Z, True, False))
+		minYpoly.append(solver(K, D, Z, False, False))
+		maxYmono.append(solver(K, D, Z, True, True))
+		minYmono.append(solver(K, D, Z, False, True))
 
+	fig, ax = plt.subplots()
+	# style of data points
+	ax.plot(X, maxYpoly, '-o', markersize = 2, color='dodgerblue')
+	ax.plot(X, minYpoly, '-o', markersize = 2, color='dodgerblue')
+	ax.plot(X, maxYmono, '-x', markersize = 2, color='red')
+	ax.plot(X, minYmono, '-x', markersize = 2, color='red')
+	ax.plot(X, attY, '-v', markersize = 2, color = 'black')
+	# predicted means
+	#ax.plot(plot_x, means_y, color = 'dodgerblue', alpha = 0.7)
+	# shade in the standard deviation around the means
+	ax.set(xlabel='X', ylabel='Y',
+	       title="Figure 6")
+	fig.savefig("fig6.png")
+	plt.show()
+	print(att)
+
+#plot_polys(19, D, Z)
+
+# debug my ATT issues
+print(solver(1, D, Z, True, False))
