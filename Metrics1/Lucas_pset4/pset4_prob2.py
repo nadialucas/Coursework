7e39731b8ce1a409e77b0b0d8c4eae3a341d8dfd
Nadia Lucas <nlucas040@gmail.com>
2020-12-05 22:04:06 -0600

    i think b is done

--- Metrics1/Lucas_pset4/pset4_prob2.py
+++ Metrics1/Lucas_pset4/pset4_prob2.py
@@ -14,62 +14,86 @@ import math
 import matplotlib.pyplot as plt
 from scipy.stats import randint
 
-
-# Construct the DGP
-N = 1000
-E = randint.rvs(2, 6, size = N).reshape(N, 1)
-T=5
-rho = .5
-theta = -2
-
-
-epsilon = np.array([ [np.random.normal() for i in range(T)]    for j in range(N) ])
-U = epsilon[:,0].reshape(N,1)
-for i in range(T-1):
-	Unext = rho*U[:,i] + epsilon[:,i+1]
-	U = np.insert(U, i+1, Unext, axis = 1)
-	
-time_matrix = np.array([ [ t for t in range(T) ] for j in range(N)])
-
-V = np.array([ [np.random.normal() for i in range(T)]    for j in range(N) ])
-
-
-Y0 = -.2 + .5*E + U
-Y1 = -.2 + .5*E + np.sin(time_matrix - theta*E) + U + V
-
+# for exact replication
+np.random.seed(1234)
 
 # an object that sets up and runs OLS and TSLS
 class estimate:
-	def __init__(self, X,Y,Z):
+	def __init__(self, X,Y):
 		self.N = len(Y)
-		self.Z = Z
 		self.Y = Y
 		self.X = X
 		self.k = X.shape[1]
 		# initialize the object [X'X]^-1. This is the "bread" of the sandwich
 		# in any sandwich estimator and will be used in each error calculation
 		self.XpXi = np.linalg.inv(np.dot(self.X.T, self.X))
-		self.ZpZi = np.linalg.inv(np.dot(self.Z.T, self.Z))
 		# solve for the coefficients
 		self.theta = np.dot(self.XpXi, np.dot(self.X.T, self.Y))
-		self.pi = np.dot(self.ZpZi, np.dot(self.Z.T, self.X))
 
 	def regress(self):
 		# returns the regular OLS estimator
 		return self.theta
 
-	def tsls(self):
-		# 2SLS estimator - following the logic of the classic sandwich estimator
-		pi = self.pi
-		PZZPinv = np.linalg.inv(np.dot(np.dot(pi.T, np.dot(self.Z.T, self.Z)),\
-		 pi)) 
-		beta = np.dot(np.dot(PZZPinv, pi.T), np.dot(self.Z.T, self.Y))
-		return beta
 
-# construct Y
-Epanel = [E for i in range(T)]
-print(Epanel.shape)
+def monte_carlo(N, M, theta, rho = 0.5):
+	rel_betas = []
+	T=5
+	rho = .5
+	for m in range(M):
+		# Construct the DGP
+		E = randint.rvs(2, 6, size = N).reshape(N, 1)
 
-# construct a very saturated regression
 
+		epsilon = np.array([ [np.random.normal() for i in range(T)]    for j in range(N) ])
+		# construct the AR1
+		U = epsilon[:,0].reshape(N,1)
+		for i in range(T-1):
+			Unext = rho*U[:,i] + epsilon[:,i+1]
+			U = np.insert(U, i+1, Unext, axis = 1)
+			
+		Tpanel = np.array([ [ t for t in range(T) ] for j in range(N)])
+
+		V = np.array([ [np.random.normal() for i in range(T)]    for j in range(N) ])
+
+
+		Y0 = -.2 + .5*E + U
+		Y1 = -.2 + .5*E + np.sin(Tpanel - theta*E) + U + V
+
+
+		# construct Y first by constructing treatment
+		Epanel = np.repeat(E, T, axis = 1)
+		D = np.array([ [1 if Epanel[i,t] <= t+1 else 0 for t in range(T)] for i in range(N) ]) 
+
+		# Then construct using potential outcomes framework
+		Y = np.multiply(D, Y1) + np.multiply(1-D, Y0)
+
+		# now we essentially 'reshape long' and stack everything
+		Ylong = Y.reshape(len(Y.flatten()), 1)
+
+
+
+		# now construct all the dummies
+		Elong = Epanel.reshape(len(Epanel.flatten()), 1)
+		Tlong = Tpanel.reshape(len(Tpanel.flatten()), 1)
+
+		cohortDums = np.array([ [1 if Elong[i] == t+2 else 0 for t in range(T-1)] for i in range(len(Elong)) ]) 
+		timeDums = np.array([ [1 if Tlong[i] == t else 0 for t in range(T)] for i in range(len(Tlong)) ]) 
+		rlist = [-3, -2, 0, 1, 2, 3]
+		relDums = np.array( [ [1 if Tlong[i]+1 - Elong[i] == r else 0 for r in rlist] for i in range(len(Tlong)) ])
+
+		# cohort fixed effects, time fixed effects, and relative time dummies are regressors
+		X = np.hstack([cohortDums, timeDums, relDums])
+
+		ols_beta = estimate(X, Ylong).regress()
+		rel_betas.append(ols_beta[9:])
+
+	betas = np.array(rel_betas)
+	mean = np.mean(betas, axis = 0)
+	low_quantile = np.quantile(betas, .025, axis = 0)
+	hi_quantile = np.quantile(betas, .975, axis=0)
+	return low_quantile, mean, hi_quantile
+
+lo1, mean1, hi1 = monte_carlo(1000, 10, -2)
+
+lo2, mean2, hi2 = monte_carlo(1000, 100, -2)
 
